5422bf92b69947f3a93f1f9bc36646e34cebb0e8
Quentin Roulland <qroulland@gmail.com>
2018-03-26 16:36:55 +0200

    Ajout bouton pr future fonctionnalitée

--- app/routes/projects.js
+++ app/routes/projects.js
@@ -7,7 +7,7 @@ export default Route.extend({
     return RSVP.hash({
       projects:this.get('store').findAll('project'),
       fields:['Name','Description',{name:'sDate',caption:'Start date'},{name:'dDate',caption:'Due date'},{name:'owner.identity',caption:'Owner'}],
-      operations:[{icon:'remove red',link:'projects.delete'},{icon:'edit',link:'projects.update'},{icon:'eye',link:'project'}]
+      operations:[{icon:'remove red',link:'projects.delete'},{icon:'edit',link:'projects.update'},{icon:'eye',link:'project'},{icon:'user',link:'project'}]
     });
   }
 });
