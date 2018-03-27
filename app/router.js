bbed15880f5dcec253eb14e8520b6bdcf9362d17
Quentin Roulland <qroulland@gmail.com>
2018-03-27 12:04:42 +0200

    Front project: ajout boutons pour sélectionner les infos a afficher

--- app/router.js
+++ app/router.js
@@ -17,7 +17,7 @@ Router.map(function() {
     this.route('update',{ path: 'update/:developer_id' });
     this.route('delete',{ path: 'delete/:developer_id' });
   });
-  this.route('project',{ path: 'project/:project_id' });
+  this.route('project', { path: 'project/:project_id' }, function() {});
 
   this.route('story', function() {
     this.route('new', {path: 'new/:project_id'});
